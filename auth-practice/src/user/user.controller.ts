6fe7e549249508625f77270b966700153ac98eee
nosferatuzodd <ahmadakhtar658@gmail.com>
2024-03-06 17:25:09 +0500

    setting up user services in nestjs for post requests"

--- auth-practice/src/user/user.controller.ts
+++ auth-practice/src/user/user.controller.ts
@@ -1,17 +1,37 @@
-import { Controller,Get,Post,Put,Delete, Body } from '@nestjs/common';
- import { createUserDto } from './dto/create-user.dto';
+/* eslint-disable prettier/prettier */
+import { Controller,Get,Post,Put,Delete, Body, Param } from '@nestjs/common';
+import { createUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
+
+
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService:UserService){}
 
-    @Get()
-findAll():string{
-    return "get request"
+@Get()
+findAll(): User[]{
+    return this.userService.findAll()
+}
+
+ 
+@Put(':id')
+update(@Body() updateUserDto:createUserDto,@Param('id')id ):string{
+    return `Updated ${id} : with ${updateUserDto}`
+}
+
+
+
+@Delete(':id')
+delete(@Param('id')id):string{
+    return `item is deleted ${id}`
 }
 
 @Post()
 create(@Body()createUserDto: createUserDto):string{
-    return `we have a name of : ${createUserDto.name}`
-    
+    return `we have a name of : ${createUserDto.name}`    
 }
+
+
 
 }
---
+++ auth-practice/src/user/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+
+@Injectable()
+export class UserService {
+  private readonly users: User[] = [
+    {
+      id: '12391488123',
+      name: 'string1',
+      email: 'string2',
+      password: 'string3',
+    },
+    {
+      id: '656669',
+      name: 'second_string',
+      email: 'third_string',
+      password: 'fourth_string',
+    }
+  ];
+
+  findAll():User[]{
+    return this.users
+  }
+
+}
